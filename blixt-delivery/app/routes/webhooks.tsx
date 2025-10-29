dbc20b007e42192e0638add3f6bcf821d5310627
Hedemo-N <niklas.hedemo@hotmail.se>
2025-10-29 13:00:53 +0100

    ändrar så att priset hämtas från supabase

--- blixt-delivery/app/routes/webhooks.tsx
+++ blixt-delivery/app/routes/webhooks.tsx
@@ -6,6 +6,7 @@ import crypto from "crypto";
 import { json, type ActionFunctionArgs } from "@remix-run/node";
 
 export async function action({ request }: ActionFunctionArgs) {
+  
   const hmacHeader = request.headers.get("x-shopify-hmac-sha256");
   const topic = request.headers.get("x-shopify-topic");
   const shop = request.headers.get("x-shopify-shop-domain");
@@ -14,6 +15,7 @@ export async function action({ request }: ActionFunctionArgs) {
   const chunks: Uint8Array[] = [];
   for await (const chunk of request.body as any) chunks.push(chunk);
   const rawBody = Buffer.concat(chunks);
+console.log("RAW BODY FULL", rawBody.toString("base64"));
 
   const generatedHash = crypto
     .createHmac("sha256", process.env.SHOPIFY_API_SECRET!)
